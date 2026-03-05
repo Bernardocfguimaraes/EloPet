e13ee922c5f174b18f952c201ba9ad6bb7447da0
Bernardo Guimarães <bernardocfguima@gmail.com>
2026-03-05 02:32:26 -0300

    Adapter no Auth configurado

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
+import type { Adapter } from "next-auth/adapters"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET!,
   trustHost: true,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [GitHub],
 })
